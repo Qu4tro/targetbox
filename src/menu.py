8dabd219f4790a7ed13f47962bc49185ffef32cc
Xavier Francisco <xavier.n.francisco@gmail.com>
2017-08-06 17:55:44 +0100

    working

--- src/menu.py
+++ src/menu.py
@@ -1,41 +1,46 @@
-from termbox import BLACK, CYAN, WHITE
+from termbox import BLACK, CYAN
 import logging as log
 
 log.basicConfig(filename='dev.log', level=log.DEBUG)
 
 
 class ListMenu:
-    def __init__(self, elements, cursor=0, wrap=True):
+    def __init__(self, elements, grid_size, cursor=0, wrap=True):
         self.elements = elements
-        self.size = len(self.elements)
-        self.window_size = None
+        self.size = len(elements)
+        self.grid_size = grid_size
+        self.window = (0, grid_size[1] - 1)
 
         self.cursor = cursor
 
         self.wrap = wrap
+        log.debug(f"# elements: {self.size}")
+        log.debug(f"window: {self.window}")
 
     @property
     def selected(self):
         return self.elements[self.cursor]
 
     def goto(self, line):
+
         def bound(pos, size):
             return 0 if pos < 0 else size - 1 if pos >= size else pos
 
         def wrap(pos, size):
             return pos % size
 
-        log.info(f"goto: {self.cursor} -> {line}")
         if self.wrap:
             self.cursor = wrap(line, self.size)
         else:
             self.cursor = bound(line, self.size)
 
-        if self.cursor != line:
-            if self.wrap:
-                log.info(f"wrapped to {self.cursor}")
-            else:
-                log.info(f"bound to {self.cursor}")
+        if self.cursor < self.window[0]:
+            self.window = (self.cursor, self.cursor + self.grid_size[1] - 1)
+        elif self.cursor > self.window[1]:
+            self.window = (self.cursor - self.grid_size[1] + 1, self.cursor)
+
+        log.debug(f"goto: {self.cursor}")
+        log.debug(f"window: {self.window}")
 
     def go(self, incr):
         self.goto(self.cursor + incr)
@@ -47,28 +52,13 @@ class ListMenu:
         self.go(1)
 
     def grid(self):
-        if self.window_size is not None:
-            (width, height) = self.window_size
-        else:
-            raise "sup"  # TODO
+        return self.color([[(ord(c), None, None)
+                            for c in element.ljust(self.grid_size[0])]
+                           for element in self.elements[self.window[0]:self.window[1] + 1]])
 
-        # TODO
-        def active_color(c):
-            return c, BLACK, CYAN
+    def color(self, grid):
+        relative_cursor = self.cursor - self.window[0]
+        grid[relative_cursor] = [(char, BLACK, CYAN)
+                                 for (char, _, _) in grid[relative_cursor]]
 
-        # TODO
-        def normal_color(c):
-            return c, WHITE, BLACK
-
-        grid = []
-        for i in range(height):
-            color = active_color if i == self.cursor else normal_color
-
-            if i < len(self.elements):
-                fmt_line = self.elements[i].ljust(width)
-                gridline = [color(ord(fmt_line[i])) for i in range(width)]
-            else:
-                gridline = [color(ord(" ")) for i in range(width)]
-
-            grid.append(gridline)
         return grid
